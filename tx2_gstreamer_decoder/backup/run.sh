dfe7c2019320a61ae593576da88f1749fa428f51
zyb2017 <15062106902@163.com>
2018-09-07 10:06:02 +0800

    delete file

--- tx2_gstreamer_decoder/backup/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gnome-terminal -x bash -c "./test"
-gnome-terminal -x bash -c "./test"
-gnome-terminal -x bash -c "./test"
-gnome-terminal -x bash -c "./test"
-gnome-terminal -x bash -c "./test"
-gnome-terminal -x bash -c "./test"
